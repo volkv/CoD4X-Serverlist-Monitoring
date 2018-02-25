d4a086a72f51e255017a4f7d1bcd2195fef00db1
Pavel Volkov <pvolkv@gmail.com>
2018-02-25 22:04:50 +0700

    Fix for no clients warning

--- index.php
+++ index.php
@@ -196,72 +196,74 @@ $baseURL = str_replace($_SERVER["DOCUMENT_ROOT"], "", DIR);
 		</span>
 	</div>
 
-	<?php foreach ($d['clients'] as $server_id => $server) { ?>
+	<?php
+	if (isset($d['clients']))
+		foreach ($d['clients'] as $server_id => $server) { ?>
 
-		<div id="c4n-mon__playerslist_<?= $server_id ?>" class="c4n-mon__playerslist">
-			<div class="c4n-mon__playerscontent">
+			<div id="c4n-mon__playerslist_<?= $server_id ?>" class="c4n-mon__playerslist">
+				<div class="c4n-mon__playerscontent">
 
-				<h2><?= uncolorize(colorize($d['servers'][$server_id])) ?></h2>
+					<h2><?= uncolorize(colorize($d['servers'][$server_id])) ?></h2>
 
-				<?php ksort($server);
-				foreach ($server as $team => $players) { ?>
+					<?php ksort($server);
+					foreach ($server as $team => $players) { ?>
 
-					<div class="c4n-mon__table_group">
-						<table class="c4n-mon__players">
-							<caption><?= $d['teams'][$server_id][$team] ?></caption>
-							<thead>
-							<tr>
-								<th><?= $lang['player'] ?></th>
-								<th><?= $lang['score'] ?></th>
-								<th><?= $lang['kills'] ?></th>
-								<th><?= $lang['deaths'] ?></th>
-								<th><?= $lang['kd'] ?></th>
-								<th><?= $lang['assists'] ?></th>
-								<th><?= $lang['ping'] ?></th>
-							</tr>
-							</thead>
-							<tbody>
-
-							<?php usort($players, 'sort_by_score');
-							foreach ($players as $player) {
-								$rank_text = get_rank_text($player['rank']); ?>
-
+						<div class="c4n-mon__table_group">
+							<table class="c4n-mon__players">
+								<caption><?= $d['teams'][$server_id][$team] ?></caption>
+								<thead>
 								<tr>
-									<td class="c4n-mon__playername">
+									<th><?= $lang['player'] ?></th>
+									<th><?= $lang['score'] ?></th>
+									<th><?= $lang['kills'] ?></th>
+									<th><?= $lang['deaths'] ?></th>
+									<th><?= $lang['kd'] ?></th>
+									<th><?= $lang['assists'] ?></th>
+									<th><?= $lang['ping'] ?></th>
+								</tr>
+								</thead>
+								<tbody>
+
+								<?php usort($players, 'sort_by_score');
+								foreach ($players as $player) {
+									$rank_text = get_rank_text($player['rank']); ?>
+
+									<tr>
+										<td class="c4n-mon__playername">
 				<span class="c4n-mon__rank-wrapper" title="<?= $rank_text ?>">
-					<span class="c4n-mon__rank-img-wrapper"><img class="c4n-mon__rank" src="<?=$baseURL ?>img/ranks/<?= get_prestige_icon($player['rank']) ?>.png"></span>
+					<span class="c4n-mon__rank-img-wrapper"><img class="c4n-mon__rank" src="<?= $baseURL ?>img/ranks/<?= get_prestige_icon($player['rank']) ?>.png"></span>
 					<sub class="c4n-mon__rank_number"><?= $player['rank'] ?></sub>
 				</span>
-										<span>	<?= $player['name'] ?></a></span>
+											<span>	<?= $player['name'] ?></a></span>
 
-									</td>
-									<td>
-										<?= $player['score'] ?>
-									</td>
+										</td>
+										<td>
+											<?= $player['score'] ?>
+										</td>
 
-									<td>
-										<?= $player['kills'] ?>
-									</td>
-									<td>
-										<?= $player['deaths'] ?>
-									</td>
-									<td>
-										<?= ($player['deaths'] > 0) ? number_format($player['kills'] / $player['deaths'], 2) : 0; ?>
-									</td>
+										<td>
+											<?= $player['kills'] ?>
+										</td>
+										<td>
+											<?= $player['deaths'] ?>
+										</td>
+										<td>
+											<?= ($player['deaths'] > 0) ? number_format($player['kills'] / $player['deaths'], 2) : 0; ?>
+										</td>
 
-									<td>
-										<?= $player['assists'] ?>
-									</td>
-									<td>
-										<?= $player['ping'] ?>
-									</td>
-								</tr>
-							<?php } ?>
-							</tbody>
-						</table>
-					</div>
-				<?php } ?>
+										<td>
+											<?= $player['assists'] ?>
+										</td>
+										<td>
+											<?= $player['ping'] ?>
+										</td>
+									</tr>
+								<?php } ?>
+								</tbody>
+							</table>
+						</div>
+					<?php } ?>
+				</div>
 			</div>
-		</div>
-	<?php } ?>
+		<?php } ?>
 </div>
